ff33edd5eccda49a2491ec314eab855267203141
karabanick <nikmunuhe@gmail.com>
2024-09-26 14:25:08 +0300

    login page modifications

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default function Login() {
+
+  const LoginUser = (e) => {
+    e.preventDefault ()
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={LoginUser}>
+      <label>Email</label>
+        <input type='email' placeholder='enter email...' />
+        <label>Password</label>
+        <input type='password' placeholder='enter password...' />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
   )
 }
