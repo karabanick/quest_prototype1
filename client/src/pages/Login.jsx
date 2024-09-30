a2e46bd2dadec651998038699391bf15255a7fee
karabanick <nikmunuhe@gmail.com>
2024-09-30 11:49:33 +0300

    client side modification

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function Login() {
+
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
 
   const LoginUser = (e) => {
     e.preventDefault ()
@@ -10,9 +15,9 @@ export default function Login() {
     <div>
       <form onSubmit={LoginUser}>
       <label>Email</label>
-        <input type='email' placeholder='enter email...' />
+        <input type='email' placeholder='enter email...' value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
         <label>Password</label>
-        <input type='password' placeholder='enter password...' />
+        <input type='password' placeholder='enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
         <button type='submit'>Login</button>
       </form>
     </div>
